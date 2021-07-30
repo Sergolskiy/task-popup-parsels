7504512fb291a0d6a092f6dc39ba04e5701123bf
artemgiant <artemgiant@gmal.com>
2021-07-30 10:23:54 +0300

    Добавил фильтр

--- js/filter.js
+++ js/filter.js
@@ -6,14 +6,16 @@ let lists     =  modalList.find('ul.delivery_list_center_list li');
 class FilterLists {
 
     static search() {
-
+        //все показываем
         lists.show();
 
+        // проверям или включены фильтры
         let isFilterSelect     = modalList.find('select.delivery_list_btn_select option:selected').val().length,
             isFilterMenu       = modalList.find('#menu').prop('checked'),
             isFilterAutomatic  = modalList.find('#automatic').prop('checked'),
-            isFilterText       = modalList.find('input[name=search]').val();
+            isFilterText       = modalList.find('input[name=search]').val().length;
 
+        // в зависимости от включенных фильтруем елементы
         if(isFilterSelect)
             this.select()
 
@@ -22,6 +24,13 @@ class FilterLists {
 
         if(isFilterAutomatic)
             this.checkbox('automatic')
+
+        if(isFilterText)
+            this.text()
+
+        // проверяем списки которые проходят под условия
+        this.whenListNotFound();
+
     }
 
     static checkbox(valueFilter) {
@@ -57,7 +66,6 @@ class FilterLists {
                     if(valueFilter.indexOf(valueList) === -1)
                         listEl.hide();
 
-            console.log(valueFilter,valueList);
 
         })
 
@@ -65,6 +73,36 @@ class FilterLists {
 
     static text() {
 
+        let valueFilter = modalList.find('input[name=search]').val().toLowerCase();
+
+        $.each(lists,function (k,list){
+
+            let listEl     =  $(list),
+                valueList  = (listEl.find('.delivery_list_center_card_title').text()
+                              + listEl.find('.delivery_list_center_card_text').text()).toLowerCase();
+
+            //когда valueList пустое
+            if( valueList === undefined || ! valueList.length)
+                listEl.hide();
+            else
+            if(valueList.indexOf(valueFilter) === -1)
+                listEl.hide();
+
+        })
+
+    }
+
+    static  whenListNotFound() {
+
+        let isListNotFound = lists.filter(function () { return $(this).css('display') !== 'none'}).length
+
+        if(! isListNotFound) {
+            modalList.find('.delivery_list_search_empty').addClass('active')
+            modalList.find('.delivery_list_center_list-wrap').removeClass('active')
+        } else {
+            modalList.find('.delivery_list_search_empty').removeClass('active')
+            modalList.find('.delivery_list_center_list-wrap').addClass('active')
+        }
     }
 }
 
