b5405053e84440e6882557055edb3cbdb5373613
Sergey <sergolskiy@gmail.com>
2022-01-23 14:58:17 +0200

    call courier fix

--- js/clarify-popup.js
+++ js/clarify-popup.js
@@ -149,12 +149,3 @@ $(document).ready(function (){
         }
     });
 })
-
-// document.querySelectorAll('textarea').forEach(el => {
-//     el.style.height = el.setAttribute('style', 'height: ' + (el.scrollHeight + 1) + 'px');
-//     el.classList.add('auto');
-//     el.addEventListener('input', e => {
-//         el.style.height = 'auto';
-//         el.style.height = (el.scrollHeight + 1) + 'px';
-//     });
-// });
